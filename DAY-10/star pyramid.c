69d82b10f1ac8fa01f57c44de2cfa6155d589359
sp4748 <sk8194966@gmail.com>
2026-06-10 10:46:35 +0530

    star pyramid

--- DAY-10/star pyramid.c
+++ DAY-10/star pyramid.c
@@ -3,8 +3,8 @@
 int main() {
     int i, j;
 
-    for(i = 5; i >= 1; i--) {
-        for(j = 1; j <= i; j++) {
+    for(i = 1; i <= 5; i++) {
+        for(j = 1; j <= (2 * i - 1); j++) {
             printf("*");
         }
         printf("\n");
