3a846cb70de3a1a82932dc4d03de7197d842e5f9
sp4748 <sk8194966@gmail.com>
2026-06-25 20:29:41 +0530

    DAY-25

---
+++ DAY-25/Sort names alphabetically.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[50][100], temp[100];
+    int n, i, j;
+
+    printf("Enter the number of names: ");
+    scanf("%d", &n);
+
+    printf("Enter %d names:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%s", names[i]);
+    }
+
+    for(i = 0; i < n - 1; i++) {
+        for(j = i + 1; j < n; j++) {
+            if(strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("\nNames in alphabetical order:\n");
+    for(i = 0; i < n; i++) {
+        printf("%s\n", names[i]);
+    }
+
+    return 0;
+}
